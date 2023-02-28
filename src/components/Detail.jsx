580e807937c1be4cff97d9af4c0d56f388f1630f
raisyaf20 <raisyafmuhamad@gmail.com>
2023-02-28 15:22:59 +0700

    modify detail components

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -14,7 +14,7 @@ const Detail = () => {
     <section className="mt-20">
       <div className="container mx-auto">
         <div className="flex bg-slate-200 p-4 rounded-lg flex-wrap justify-center md:bg-inherit md:rounded-none lg:flex-row">
-          <div className="w-[20rem] px-4 md:w-1/2">
+          <div className="w-[20rem] px-4 md:w-1/3">
             <img
               src={item.thumbnail}
               alt="laptop"
@@ -22,8 +22,29 @@ const Detail = () => {
             />
           </div>
           <div className="w-[23rem] mt-4 text-center px-4 md:w-1/2">
-            <div className="flex justify-end">
-              <p>{item.rating}</p>
+            <div className="flex justify-end items-center">
+              <p className="mr-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-star-filled text-yellow-300"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+                    stroke-width="0"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+              </p>
+              <p className="text-base text-slate-700">{item.rating}</p>
             </div>
             <p className="text-base mb-1">{item.brand}</p>
             <h1 className="text-3xl text-gray-800 mb-1 font-semibold">
